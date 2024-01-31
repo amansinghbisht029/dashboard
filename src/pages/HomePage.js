44ad3c1c32722d3b40cdbb99c14ce4b09801abc8
amansinghbisht029 <83057532+amansinghbisht029@users.noreply.github.com>
2024-01-31 17:26:07 +0530

    homepage added

---
+++ src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+      <div className="w-1/2	float-left bg-[#605BFF] origin-bottom-right skew-x-3"></div>
+
+      <div className="w-1/2	float-right"></div>
+    </div>
+  );
+}
+
+export default HomePage
